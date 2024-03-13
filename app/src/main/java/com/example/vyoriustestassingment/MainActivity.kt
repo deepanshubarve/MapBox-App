8c0e8ce80ff31b695581235d3fd0152a657578a6
Deepanshu <Synosuid69@gmail.com>
2024-03-13 13:01:42 +0530

    First commit

--- app/src/main/java/com/example/vyoriustestassingment/MainActivity.kt
+++ app/src/main/java/com/example/vyoriustestassingment/MainActivity.kt
@@ -1,25 +1,72 @@
 package com.example.vyoriustestassingment
 
+import android.graphics.Point
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.MapView
+import com.mapbox.maps.CameraOptions
+
+
 
 class MainActivity : AppCompatActivity() {
+    private var mapView: MapView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        mapView= findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
 
-
-
+        mapView?.getMapAsync{mapboxMap->
 
 
         }
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+
+        mapView?.onSaveInstanceState(outState)
+
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
 }
+
+
+
+
+
